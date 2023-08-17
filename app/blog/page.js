f52c9e5c9d30cb58d46ff828ac85f2fa5943ea98
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-17 17:05:39 +0900

    ブログページの作成
    data配下のmdファイルの一覧を表示するところまで学習完了

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,7 +1,14 @@
+import fs from "fs";
+import path from "path";
+
+async function getAllBlogs() {
+  const files = fs.readdirSync(path.join("data"));
+  console.log(files);
+}
+
 const Blog = () => {
-    return (
-        <h1>ブログページ</h1>
-    );
+  getAllBlogs();
+  return <h1>ブログページ</h1>;
 };
 
-export default Blog
+export default Blog;
