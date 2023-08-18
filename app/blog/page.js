03512c38b2900717f608b8210d5a41c7b5122460
iruma-tea <shusuke.think.pad@gmail.com>
2023-08-19 07:46:41 +0900

    記事ページのリンク
    リンク生成ロジックを追加
    [slug]汎用フォルダ/Pageの作成開始

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,17 +1,21 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import Link from "next/link";
 
 async function getAllBlogs() {
   const files = fs.readdirSync(path.join("data"));
   // console.log(files);
 
   const blogs = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
     const fileData = fs.readFileSync(path.join("data", filename), "utf-8");
     // console.log(fileData);
     const { data } = matter(fileData);
     return {
       frontmatter: data,
+      slug: slug,
     };
   });
   // blogs;
@@ -32,6 +36,7 @@ const Blog = async () => {
         <div key={index}>
           <h2>{blog.frontmatter.title}</h2>
           <p>{blog.frontmatter.date}</p>
+          <Link href={`/blog/${blog.slug}`}>Read More</Link>
         </div>
       ))}
     </div>
